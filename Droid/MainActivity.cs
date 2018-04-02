bca5e00d3bb48a49ef7c8a5c17feca221cbfd563
JW <JW@DESKTOP-SOGB1LT>
2018-04-01 20:25:30 -0430

    Revert "primer cambio"
    This reverts commit 3c57eb971e8b256fe985925e05b64a696f1b9d70.

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -2,10 +2,6 @@
 using Android.Content.PM;
 using Android.OS;
 
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Analytics;
-using Microsoft.AppCenter.Crashes;
-
 namespace TodoAzure.Droid
 {
     [Activity(Label = "TodoAzure.Droid",
@@ -18,10 +14,6 @@ namespace TodoAzure.Droid
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
-
-            AppCenter.Start("f3ee7f94-b3e6-4213-aeb4-a3158f206c93",
-                   typeof(Analytics), typeof(Crashes));
-            AppCenter.Start("f3ee7f94-b3e6-4213-aeb4-a3158f206c93", typeof(Analytics), typeof(Crashes));
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -3920,6 +3920,27 @@ namespace TodoAzure.Droid
 			// aapt resource value: 0x7f08000f
 			public const int common_signin_button_text_long = 2131230735;
 			
+			// aapt resource value: 0x7f080054
+			public const int default_web_client_id = 2131230804;
+			
+			// aapt resource value: 0x7f080055
+			public const int firebase_database_url = 2131230805;
+			
+			// aapt resource value: 0x7f080052
+			public const int gcm_defaultSenderId = 2131230802;
+			
+			// aapt resource value: 0x7f080056
+			public const int google_api_key = 2131230806;
+			
+			// aapt resource value: 0x7f080053
+			public const int google_app_id = 2131230803;
+			
+			// aapt resource value: 0x7f080057
+			public const int google_crash_reporting_api_key = 2131230807;
+			
+			// aapt resource value: 0x7f080058
+			public const int google_storage_bucket = 2131230808;
+			
 			// aapt resource value: 0x7f080011
 			public const int mr_button_content_description = 2131230737;
 			
