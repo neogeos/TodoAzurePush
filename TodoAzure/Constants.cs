8083e888323d72bcb0e2882a6d82f45f7152820d
JW <JW@DESKTOP-SOGB1LT>
2018-04-01 19:30:40 -0430

    Agregar archivos de proyecto.

---
+++ TodoAzure/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace TodoAzure
+{
+    public static class Constants
+    {
+        // Replace string with your mobile service URL.
+        public static string ApplicationURL = @"https://xamarinazuredroidservice.azurewebsites.net";
+    }
+}
